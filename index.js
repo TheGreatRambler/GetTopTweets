5f4361cc473171449984149e4d802823e00b5b21
TheGreatRambler <aehart684351@gmail.com>
2021-01-21 20:19:56 -0500

    Begin to add top tweets banner

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const Twitter = require ("twitter");
-const config  = require ("./config.json");
-const fs      = require ("fs");
-const ChartJS = require("chartjs-node-canvas");
-const Canvas = require("canvas");
+const Twitter   = require ("twitter");
+const config    = require ("./config.json");
+const fs        = require ("fs");
+const ChartJS   = require ("chartjs-node-canvas");
+const Canvas    = require ("canvas");
+const puppeteer = require ("puppeteer");
 
 // Consider looking into this https://pypi.org/project/GetOldTweets3/
 
 var client;
 
 var allResponses        = [];
-var maxAllResponsesSize = -1;
+var maxAllResponsesSize = 30;
 
 var allTweetsPath = "./tweets.csv";
 
@@ -52,10 +53,11 @@ async function getEveryTweet (username) {
 
 			responses.forEach(function (tweet) {
 				if (!tweet.text.startsWith("RT ")) {
+					tweet.url = `https://twitter.com/${username}/status/${tweet.id_str}`;
 					allResponses.push(tweet);
 
 					// All charactors except spaces must be replaced
-					allTweets.write(`"${tweet.text.replace(/\n/g, "%0A").replace(/"/g, "%22")}",${tweet.favorite_count},${tweet.retweet_count},${tweet.id_str},${tweet.created_at},https://twitter.com/${username}/status/${tweet.id_str}\n`);
+					allTweets.write(`"${tweet.text.replace(/\n/g, "%0A").replace(/"/g, "%22")}",${tweet.favorite_count},${tweet.retweet_count},${tweet.id_str},${tweet.created_at},tweet.url\n`);
 				}
 			});
 
@@ -66,10 +68,6 @@ async function getEveryTweet (username) {
 		}
 	}
 
-	allResponses.forEach(function (tweet) {
-
-	});
-
 	allTweets.close();
 }
 
@@ -79,4 +77,36 @@ client = new Twitter ({
 	bearer_token: config.Bearer_token
 });
 
-getEveryTweet ("connnor01");
+(async () => {
+	getEveryTweet ("simplyn64");
+
+	const browser = await puppeteer.launch({
+		headless: true
+	});
+
+	Canvas.registerFont("assets/HelveticaNeue-Bold.ttf", {
+		family: "HelveticaNeue",
+		style: "bold"
+	});
+
+	Canvas.registerFont("assets/HelveticaNeue-Regular.ttf", {
+		family: "HelveticaNeue",
+		style: "normal"
+	});
+
+	const canvas = Canvas.createCanvas(1000, 600);
+	const ctx    = canvas.getContext("2d");
+
+	var userInfo       = allResponses[0].user;
+	var profilePicture = userInfo.profile_image_url.replace("normal", "400x400");
+
+	for (let i = 0; i < allResponses.length; i++) {
+		var tweet = allResponses[i];
+
+		// https://twitter.com/MatttGFX/status/1352400305963618306?s=20
+		const page = await browser.newPage();
+		await page.goto(tweet.url + "?s=20");
+	}
+
+	browser.close();
+}) ();

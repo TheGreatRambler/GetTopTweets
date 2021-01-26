3b6b87393f1fdc348f02bc577dab51afdfe09924
TheGreatRambler <TheGreatRambler>
2021-01-26 01:50:11 -0500

    Fix tweet processing from twint

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-const Twitter    = require ("twitter");
-const config     = require ("./config.json");
-const fs         = require ("fs");
-const ChartJS    = require ("chartjs-node-canvas");
-const Canvas     = require ("canvas");
-const puppeteer  = require ("puppeteer");
-const CP         = require ("child_process");
-const lineReader = require ("line-reader");
+const Twitter    = require("twitter");
+const config     = require("./config.json");
+const fs         = require("fs");
+const ChartJS    = require("chartjs-node-canvas");
+const Canvas     = require("canvas");
+const puppeteer  = require("puppeteer");
+const CP         = require("child_process");
+const lineReader = require("line-reader");
 
 // Consider looking into this https://pypi.org/project/GetOldTweets3/
 
 var client;
 
-var allResponses  = [];
 var temporaryPath = "tweets.txt"
-var tweetLimit    = -1;
+var tweetLimit    = 30;
 
-if (fs.existsSync(temporaryPath)) {
+if(fs.existsSync(temporaryPath)) {
 	fs.unlinkSync(temporaryPath);
 }
 
-async function getEveryTweet (username) {
-	// 9223372036854775807 is the maximum number in python, so it's basically infinity
-	CP.execSync(`twint -u ${username} --format "{'link':'{link}','tweet':'{tweet}','replies':{replies},'retweets':{retweets},'likes':{likes},'id':'{id}','date':'{date}'}" -o ${temporaryPath} --limit ${tweetLimit == -1 ? 9223372036854775807 : tweetLimit}`);
+async function getEveryTweet(username) {
+	// 9223372036854775807 is the maximum number in python, so it's basically
+	// infinity
+	var cmdLine = `twint -u ${
+		username} --format "{link}|{tweet}|{replies}|{retweets}|{likes}|{id}|{date}" -o ${
+		temporaryPath} --limit ${
+		tweetLimit == -1 ? 9223372036854775807 : tweetLimit}`;
+	console.log(cmdLine);
+	CP.execSync(cmdLine, { stdio: 'ignore' });
 }
 
-client = new Twitter ({
+client = new Twitter({
 	consumer_key: config.API_key,
 	consumer_secret: config.API_secret_key,
 	bearer_token: config.Bearer_token
@@ -32,43 +37,57 @@ client = new Twitter ({
 
 (async () => {
 	var username = "simplyn64"
-	getEveryTweet (username);
+	getEveryTweet(username);
+
+	console.log("Done scraping tweets, now processing");
 
 	var tweets = [];
-	lineReader.eachLine(temporaryPath, function (line) {
-		if (line !== "") {
-			tweets.push(JSON.parse(line));
+	lineReader.eachLine(temporaryPath, function(line) {
+		if(line !== "") {
+			var parts = line.split("|");
+			tweets.push({
+				link: parts[0],
+				tweet: parts[1],
+				replies: parseInt(parts[2]),
+				retweets: parseInt(parts[3]),
+				likes: parseInt(parts[4]),
+				id: parts[5],
+				date: parts[6]
+			});
 		}
 	});
 
-	const browser = await puppeteer.launch({
-		headless: true
-	});
+	const browser = await puppeteer.launch({ headless: true });
 
-	Canvas.registerFont("assets/HelveticaNeue-Bold.ttf", {
-		family: "HelveticaNeue",
-		style: "bold"
-	});
+	Canvas.registerFont("assets/HelveticaNeue-Bold.ttf",
+		{ family: "HelveticaNeue", style: "bold" });
 
-	Canvas.registerFont("assets/HelveticaNeue-Regular.ttf", {
-		family: "HelveticaNeue",
-		style: "normal"
-	});
+	Canvas.registerFont("assets/HelveticaNeue-Regular.ttf",
+		{ family: "HelveticaNeue", style: "normal" });
 
 	const canvas = Canvas.createCanvas(1000, 600);
 	const ctx    = canvas.getContext("2d");
 
-	var userInfo       = JSON.parse(CP.execSync(`twint -u ${username} --user-full --format "{'name':'{name}','bio':'{bio}','tweets':{tweets},'following':{following},'followers':{followers},'avatar':'{avatar}'}"`));
-    var profilePicture = userInfo.avatar.replace("normal", "400x400");
+	var userParts = CP.execSync(`twint -u ${username} --user-full --format "{name}|{bio}|{tweets}|{following}|{followers}|{avatar}"`).toString("utf8").split("|");
+	var userInfo       = {
+		name: userParts[0],
+		bio: userParts[1],
+		tweets: parseInt(userParts[2]),
+		following: parseInt(userParts[3]),
+		followers: parseInt(userParts[4]),
+		avatar: userParts[5].replace("\n", "").replace("normal", "400x400")
+	};
+	console.log(userInfo);
 
-	for (let i = 0; i < allResponses.length; i++) {
-		var tweet = allResponses[i];
+	for(let i = 0; i < tweets.length; i++) {
+		var tweet = tweets[i];
 		console.log(tweet);
 
 		// https://twitter.com/MatttGFX/status/1352400305963618306?s=20
-		const page = await browser.newPage();
-		await page.goto(tweet.url + "?s=20");
+		//const page = await browser.newPage();
+		console.log(tweet.link + "?s=20");
+		//await page.goto(tweet.link + "?s=20");
 	}
 
 	browser.close();
-}) ();
+})();
